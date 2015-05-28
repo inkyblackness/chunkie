69f41a6a736fd1c66c972db3cd93db4f3767c26c
Christian Haas <christian.haas@sevensuns.at>
2015-05-28 17:05:05 +0200

    Added import function, added .wav conversion

--- README.md
+++ README.md
@@ -8,7 +8,8 @@ This is a tool as part of the [InkyBlackness](https://inkyblackness.github.io) p
 
 ```
 Usage:
-  chunkie export <resource-file> <chunk-id> [--block=<block-id>] [<folder>]
+  chunkie export <resource-file> <chunk-id> [--block=<block-id>] [--raw] [<folder>]
+  chunkie import <resource-file> <chunk-id> [--block=<block-id>] [--data-type=<id>] <source-file>
   chunkie -h | --help
   chunkie --version
 
@@ -16,12 +17,18 @@ Options:
   <resource-file>     The resource file to work on.
   <chunk-id>          The chunk identifier. Defaults to decimal, use "0x" as prefix for hexadecimal.
   --block=<block-id>  The block identifier. Defaults to decimal, use "0x" as prefix for hexadecimal. [default: 0]
+	--raw               With this flag, the chunk will be exported without conversion to a common file format.
+	--data-type=<id>    The type of the chunk to write.
   <folder>            The path of the folder to use. [default: .]
+	<source-file>       The source file to import.
   -h --help           Show this screen.
   --version           Show version.
 ```
 
 The base file name of files is ```XXXX_YYY.ZZZ```. XXXX is the hexadecimal presentation of the chunk number. YYY is decimal for the block number. ZZZ is the type of the file, defaulting to ```bin```.
+
+For exporting, audio chunks are saved as .wav files. Specifying --raw will export the chunk in its raw format.
+Files are imported raw as well, unless a conversion is known. This is the case for .wav files when writing sound chunks.
 
 ## License
 
--- main.go
+++ main.go
@@ -10,8 +10,12 @@ import (
 	docopt "github.com/docopt/docopt-go"
 
 	"github.com/inkyblackness/res"
+	"github.com/inkyblackness/res/audio"
 	"github.com/inkyblackness/res/chunk"
 	"github.com/inkyblackness/res/chunk/dos"
+	"github.com/inkyblackness/res/serial"
+
+	"github.com/inkyblackness/chunkie/conv/wav"
 )
 
 const (
@@ -22,6 +26,28 @@ const (
 	// Title contains a combined string of name and version
 	Title = Name + " v." + Version
 )
+
+func usage() string {
+	return Title + `
+
+Usage:
+  chunkie export <resource-file> <chunk-id> [--block=<block-id>] [--raw] [<folder>]
+  chunkie import <resource-file> <chunk-id> [--block=<block-id>] [--data-type=<id>] <source-file>
+  chunkie -h | --help
+  chunkie --version
+
+Options:
+  <resource-file>     The resource file to work on.
+  <chunk-id>          The chunk identifier. Defaults to decimal, use "0x" as prefix for hexadecimal.
+  --block=<block-id>  The block identifier. Defaults to decimal, use "0x" as prefix for hexadecimal. [default: 0]
+	--raw               With this flag, the chunk will be exported without conversion to a common file format.
+	--data-type=<id>    The type of the chunk to write.
+  <folder>            The path of the folder to use. [default: .]
+	<source-file>       The source file to import.
+  -h --help           Show this screen.
+  --version           Show version.
+`
+}
 
 func main() {
 	arguments, _ := docopt.Parse(usage(), nil, true, Title, false)
@@ -34,35 +60,92 @@ func main() {
 		provider, _ := dos.NewChunkProvider(inFile)
 		chunkID, _ := strconv.ParseUint(arguments["<chunk-id>"].(string), 0, 16)
 		blockID, _ := strconv.ParseUint(arguments["--block"].(string), 0, 16)
-		folder := arguments["<folder>"].(string)
+		raw := arguments["--raw"].(bool)
+		folderArgument := arguments["<folder>"]
+		folder := "."
 
+		if folderArgument != nil {
+			folder = folderArgument.(string)
+		}
 		os.MkdirAll(folder, os.FileMode(0755))
-		export(provider, folder, res.ResourceID(chunkID), uint16(blockID))
+
+		holder := provider.Provide(res.ResourceID(chunkID))
+		outFileName := fmt.Sprintf("%04X_%03d", int(chunkID), blockID)
+		export(holder, uint16(blockID), path.Join(folder, outFileName), raw)
+	} else if arguments["import"].(bool) {
+		resourceFile := arguments["<resource-file>"].(string)
+		chunkID, _ := strconv.ParseUint(arguments["<chunk-id>"].(string), 0, 16)
+		blockID, _ := strconv.ParseUint(arguments["--block"].(string), 0, 16)
+		sourceFile := arguments["<source-file>"].(string)
+		dataType := -1
+		dataTypeArgument := arguments["--data-type"]
+		if dataTypeArgument != nil {
+			result, _ := strconv.ParseUint(dataTypeArgument.(string), 0, 8)
+			dataType = int(result)
+		}
+
+		importData(resourceFile, res.ResourceID(chunkID), uint16(blockID), dataType, sourceFile)
 	}
 }
 
-func export(provider chunk.Provider, folder string, chunkID res.ResourceID, blockID uint16) {
-	holder := provider.Provide(chunkID)
+func export(holder chunk.BlockHolder, blockID uint16, outFileName string, raw bool) {
 	blockData := holder.BlockData(blockID)
 
-	outFileName := fmt.Sprintf("%04X_%03d.bin", int(chunkID), blockID)
-	ioutil.WriteFile(path.Join(folder, outFileName), blockData, os.FileMode(0644))
+	if !raw && holder.ContentType() == res.Sound {
+		soundData, _ := audio.DecodeSoundChunk(blockData)
+		wav.ExportToWav(outFileName+".wav", soundData)
+	} else {
+		ioutil.WriteFile(outFileName+".bin", blockData, os.FileMode(0644))
+	}
 }
 
-func usage() string {
-	return Title + `
+func importData(resourceFile string, chunkID res.ResourceID, blockID uint16, dataType int, sourceFile string) {
+	buffer := serial.NewByteStore()
+	writer := dos.NewChunkConsumer(buffer)
 
-Usage:
-  chunkie export <resource-file> <chunk-id> [--block=<block-id>] [<folder>]
-  chunkie -h | --help
-  chunkie --version
+	{
+		inFile, _ := os.Open(resourceFile)
+		defer inFile.Close()
+		provider, _ := dos.NewChunkProvider(inFile)
 
-Options:
-  <resource-file>     The resource file to work on.
-  <chunk-id>          The chunk identifier. Defaults to decimal, use "0x" as prefix for hexadecimal.
-  --block=<block-id>  The block identifier. Defaults to decimal, use "0x" as prefix for hexadecimal. [default: 0]
-  <folder>            The path of the folder to use. [default: "."]
-  -h --help           Show this screen.
-  --version           Show version.
-`
+		ids := provider.IDs()
+		for _, id := range ids {
+			sourceChunk := provider.Provide(id)
+			blockCount := sourceChunk.BlockCount()
+			blocks := make([][]byte, blockCount)
+			for block := uint16(0); block < blockCount; block++ {
+				if id == chunkID && block == blockID {
+					blocks[block] = importFile(sourceFile, sourceChunk.ContentType())
+				} else {
+					blocks[block] = sourceChunk.BlockData(block)
+				}
+			}
+
+			destChunk := chunk.NewBlockHolder(sourceChunk.ChunkType(), sourceChunk.ContentType(), blocks)
+			writer.Consume(id, destChunk)
+		}
+	}
+	writer.Finish()
+
+	err := ioutil.WriteFile(resourceFile, buffer.Data(), os.FileMode(0644))
+	if err != nil {
+		panic(err)
+	}
+}
+
+func importFile(sourceFile string, dataType res.DataTypeID) (data []byte) {
+	extension := path.Ext(sourceFile)
+	switch extension {
+	case ".wav":
+		{
+			soundData := wav.ImportFromWav(sourceFile)
+			data = audio.EncodeSoundChunk(soundData)
+		}
+	default:
+		{
+			data, _ = ioutil.ReadFile(sourceFile)
+		}
+	}
+
+	return
 }
